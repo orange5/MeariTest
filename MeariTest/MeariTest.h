1efb59e3e518c1f8c098ec06dfc1d81933512cf0
xiaoju.zhuo <xiaojuzhuo@gmail.com>
2020-08-03 14:03:22 +0800

    Initial Commit

---
+++ MeariTest/MeariTest.h
@@ -0,0 +1,19 @@
+//
+//  MeariTest.h
+//  MeariTest
+//
+//  Created by xj zhuo on 2020/8/3.
+//  Copyright © 2020 xj zhuo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MeariTest.
+FOUNDATION_EXPORT double MeariTestVersionNumber;
+
+//! Project version string for MeariTest.
+FOUNDATION_EXPORT const unsigned char MeariTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MeariTest/PublicHeader.h>
+
+
