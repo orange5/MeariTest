9e2ae16ca111e9aa52729dadc3f659c10d689ae3
xiaoju.zhuo <xiaojuzhuo@gmail.com>
2020-08-03 14:33:13 +0800

    Modify

--- MeariTest/MeariTest.h
+++ MeariTest/MeariTest.h
@@ -16,9 +16,9 @@ FOUNDATION_EXPORT const unsigned char MeariTestVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MeariTest/PublicHeader.h>
 
-@interface MeariTest
+@interface MeariTest : NSObject
 
 - (void)testString;
 
-@end MeariTest
+@end
 
